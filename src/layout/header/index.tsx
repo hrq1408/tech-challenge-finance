ded391410599a08143756a2b1322feb123d889dc
Otavio Augusto <otavioaufegues@gmail.com>
2025-10-26 16:27:25 -0300

    feat: layout header

---
+++ src/layout/header/index.tsx
@@ -0,0 +1,23 @@
+import Link from "next/link";
+
+export function Header() {
+  return (
+    <header className="flex px-20 py-4 bg-primary text-white">
+        <div className="flex items-center justify-between w-full mx-auto max-w-7x1">
+            <div>
+               Bem vindo João!
+            </div>
+            <nav>
+                <ul className="flex gap-2 items-center justify-center">
+                    <li>
+                        <Link href="/">Home</Link>
+                    </li>
+                    <li>
+                        <Link href="/transactions">Transações</Link >
+                    </li>
+                </ul>
+            </nav>
+        </div>
+    </header>
+  );
+}
